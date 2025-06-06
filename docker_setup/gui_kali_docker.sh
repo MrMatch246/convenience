3bc9e6ddb62d606b59ae72daf7d11416c5ced39b
MrMatch246 <daniel.erceg@aol.de>
2025-06-06 19:29:40 +0200

    burp

--- docker_setup/gui_kali_docker.sh
+++ docker_setup/gui_kali_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-apt update && apt -y install kali-linux-default zsh fonts-powerline
+apt update && apt -y install kali-linux-default zsh fonts-powerline lnav
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' /root/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' /root/.zshrc
 cp $SCRIPT_DIR/../zsh_setup/aliases/docker_aliases.zsh /root/.oh-my-zsh/custom/docker_aliases.zsh
+setcap cap_net_raw,cap_net_bind_service+eip /usr/lib/nmap/nmap
 zsh
--- zsh_setup/aliases/docker_aliases.zsh
+++ zsh_setup/aliases/docker_aliases.zsh
@@ -1,1 +1,2 @@
 alias zenmap="nohup zenmap > /dev/null 2>&1 & disown"
+alias burp="nohup /opt/BurpSuitePro/BurpSuitePro > /dev/null 2>&1 & disown"
