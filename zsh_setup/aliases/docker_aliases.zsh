39bb04d49883519ade7fbca72730697ade78a5e3
MrMatch246 <daniel.erceg@aol.de>
2025-06-08 23:36:32 +0200

    updated docker aliases

--- zsh_setup/aliases/docker_aliases.zsh
+++ zsh_setup/aliases/docker_aliases.zsh
@@ -1,3 +1,4 @@
+alias upali="cp /root/convenience/zsh_setup/aliases/docker_aliases.zsh /root/.oh-my-zsh/custom/docker_aliases.zsh;source ~/.zshrc"
 alias zenmap="nohup zenmap > /dev/null 2>&1 & disown"
 alias burp="nohup /opt/BurpSuitePro/BurpSuitePro --user-config-file ~/tmp/default_burp_user_settings.json > /dev/null 2>&1 & disown"
 alias msf="service postgresql start;msfdb run"
