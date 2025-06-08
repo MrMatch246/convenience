2579399c43bbb6a085c2d425bb73d60d2a594e6e
MrMatch246 <daniel.erceg@aol.de>
2025-06-08 18:59:03 +0200

    Tab completion sorts by container creation date

--- zsh_setup/aliases/project_aliases.zsh
+++ zsh_setup/aliases/project_aliases.zsh
@@ -1,5 +1,5 @@
 alias uproj="cp ~/REPOS/convenience/zsh_setup/aliases/project_aliases.zsh ~/.oh-my-zsh/custom/project_aliases.zsh;source ~/.zshrc"
-# Completion function for "project enter"
+# Completion function for "project"
 _project() {
   local curcontext="$curcontext" state line
   typeset -A opt_args
@@ -10,8 +10,14 @@ _project() {
 
   if [[ $words[2] == (enter|switch|archive|remove|export) ]]; then
     local containers
-    containers=(${(f)"$(docker ps -a --format '{{.Names}}')"})
-    _describe 'containers' containers
+
+    containers=(${(f)"$(
+      docker ps -a --format '{{.CreatedAt}} {{.Names}}' |
+      sort -r |
+      awk '{ $1=$2=$3=$4=""; sub(/^ +/, ""); print }'
+    )"})
+
+    _describe 'containers (newest first)' containers
   fi
 }
 
