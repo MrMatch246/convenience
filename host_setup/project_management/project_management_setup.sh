8c63f69c4df8ef59dccd9c530ae5d9448b80a8aa
MrMatch246 <daniel.erceg@aol.de>
2025-07-09 12:56:06 +0200

    Fixed filetype and missing dir

--- host_setup/project_management/project_management_setup.sh
+++ host_setup/project_management/project_management_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p /home/$(whoami)/.local/bin
 ln -s "$SCRIPT_DIR/project.sh" /home/$(whoami)/.local/bin/project
 chmod +x /home/$(whoami)/.local/bin/project
-cp "$SCRIPT_DIR/project.zsh" /home/$(whoami)/.oh-my-zsh/custom/plugins/project/project.zsh
 cp $SCRIPT_DIR/../../zsh_setup/aliases/project_aliases.zsh /home/$(whoami)/.oh-my-zsh/custom/project_aliases.zsh
