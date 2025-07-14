e731f18e4f94c88f4c5d38f0f7f5c7766e31d6ae
MrMatch246 <daniel.erceg@aol.de>
2025-07-14 10:49:34 +0200

    added bin appending to project_management_setup.sh

--- host_setup/project_management/project_management_setup.sh
+++ host_setup/project_management/project_management_setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LINE='export PATH="$PATH:/home/user/.local/bin"'
+ZSHRC="$HOME/.zshrc"
 mkdir -p /home/$(whoami)/.local/bin
 ln -sf "$SCRIPT_DIR/project.sh" /home/$(whoami)/.local/bin/project
 chmod +x /home/$(whoami)/.local/bin/project
 cp $SCRIPT_DIR/../../zsh_setup/aliases/project_aliases.zsh /home/$(whoami)/.oh-my-zsh/custom/project_aliases.zsh
+if ! grep -Fxq "$LINE" "$ZSHRC"; then
+  echo "$LINE" >> "$ZSHRC"
+fi
