2c3b75388266ec1c9c5c1d993d05468a8085fd33
MrMatch246 <daniel.erceg@aol.de>
2025-07-22 17:47:31 +0200

    aliases

--- zsh_setup/aliases/kali_vm_aliases.zsh
+++ zsh_setup/aliases/kali_vm_aliases.zsh
@@ -12,4 +12,4 @@ alias msf="service postgresql start;msfdb run"
 alias burp="nohup /opt/BurpSuitePro/BurpSuitePro --user-config-file=~/tmp/default_burp_user_settings.json > /dev/null 2>&1 & disown"
 alias zap="nohup zap.sh > /dev/null 2>&1 & disown"
 alias arse='arsenal'
-alias argu='source ~/REPOS/argus/env/bin/activate && python3 /root/argus/argus.py'
+alias argu='source ~/REPOS/argus/env/bin/activate && python3 ~/REPOS/argus/argus.py'
